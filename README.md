dae64a523d1e562feb654840a2c9c4cb8ae95b99
syoamakase <syoamakase@gmail.com>
2020-07-17 14:32:59 +0900

    add utils

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Transformer_TTS
+
+This repository is for Text-to-Speech (TTS) project.
+
+
+# TODO
+- add preprocess of LJSpeech (En) and JSUT (Ja)
+- add FastSpeech2
---
+++ utils/average_checkpoints.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+
+import argparse
+import json
+import os
+
+import numpy as np
+
+def main():
+
+    if args.start is None and args.end is None:
+        print('average {} files from last modified model'.format(args.num))
+        last = sorted(args.snapshots, key=os.path.getmtime)
+        last = last[-args.num:]
+    elif args.start is not None and args.end is not None:
+        last = []
+        dirname = os.path.dirname(args.snapshots[0])
+        for epoch in range(args.start, args.end+1):
+            last.append(os.path.join(dirname, 'network.epoch{}'.format(epoch)))
+        
+    print("average over", last)
+    avg = None
+
+    if args.backend == 'pytorch':
+        import torch
+        # sum
+        for path in last:
+            print(path)
+            #states = torch.load(path, map_location=torch.device("cpu"))["model"]
+            states = torch.load(path, map_location=torch.device("cpu"))
+            if avg is None:
+                avg = states
+            else:
+                for k in avg.keys():
+                    avg[k] += states[k]
+
+        # average
+        for k in avg.keys():
+            if avg[k] is not None:
+                avg[k] /= args.num
+
+        torch.save(avg, args.out)
+        print('{} saved.'.format(args.out))
+
+    else:
+        raise ValueError('Incorrect type of backend')
+
+
+def get_parser():
+    parser = argparse.ArgumentParser(description='average models from snapshot')
+    parser.add_argument("--snapshots", required=True, type=str, nargs="+")
+    parser.add_argument("--out", required=True, type=str)
+    parser.add_argument("--num", default=10, type=int)
+    parser.add_argument("--start", default=None, type=int)
+    parser.add_argument("--end", default=None, type=int)
+    parser.add_argument("--backend", default='pytorch', type=str)
+    return parser
+
+if __name__ == '__main__':
+    args = get_parser().parse_args()
+    main()
